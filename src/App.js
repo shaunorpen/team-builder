3728c30805043293416688e749364f4b3d51a79b
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-18 13:31:51 +0100

    Add initial structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import uuid from 'uuid';
 import './App.css';
+
+const initialTeamMembers = [
+  {id: uuid(), name: 'Shaun', age: '37', title: 'Manager'},
+  {id: uuid(), name: 'Dave', age: '35', title: 'Coach'},
+  {id: uuid(), name: 'Justin', age: '40', title: 'Captain'},
+];
+
+function TeamMember({teamMember}) {
+  return (
+    <div>
+      {`${teamMember.name} is ${teamMember.age} and acts as ${teamMember.title}`}
+    </div>
+  );
+}
+
+function TeamList({ teamMembers }) {
+  return (
+    teamMembers.map(teamMember => <TeamMember teamMember={teamMember} />)
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div className="App">
+        <h2>Team List</h2>
+        <TeamList teamMembers={initialTeamMembers} />
+      </div>
+      <form>
+        <div><label htmlFor='nameInput'>Name</label>
+        <input type='text' id='nameInput' /></div>
+        <div><label htmlFor='ageInput'>Age</label>
+        <input type='text' id='ageInput' /></div>
+        <div><label htmlFor='titleInput'>Title</label>
+        <input type='text' id='titleInput' /></div>
+        <button>Submit</button>
+      </form>
+    </>
   );
 }
 
